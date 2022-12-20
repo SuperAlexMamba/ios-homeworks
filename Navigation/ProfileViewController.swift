ca835bf5d3eefef736021c530feaef4b886c8fbc
Vyacheslav <slava.orlob@gmail.com>
2022-12-21 00:52:48 +0300

    Initial Commit

---
+++ Navigation/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Слава Орлов on 19.12.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        title = "Profile"
+        
+    }
+}
